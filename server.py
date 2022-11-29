291d395ffce08fc686a4fe6a3d0626206b5dbe56
Kevie <65157770+Kevsay@users.noreply.github.com>
2022-11-29 10:43:50 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,11 +4,13 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 server_ip = '' # RPi IP ADDRESS
 server_port = 1011
 
+RPi_port = 18
+
 status = None
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(RPi_port, GPIO.OUT)
 
 class MyServer(BaseHTTPRequestHandler):
   def do_POST(self):
@@ -19,12 +21,12 @@ class MyServer(BaseHTTPRequestHandler):
       self.send_response(304)
      
     elif state == 'true':
-      GPIO.output(18, GPIO.HIGH)
+      GPIO.output(RPi_port, GPIO.HIGH)
       self.send_response(201)
       status = 'true'
       
     elif state == 'false':
-      GPIO.output(18, GPIO.LOW)
+      GPIO.output(RPi_port, GPIO.LOW)
       self.send_response(200)
       status = 'false'
       
